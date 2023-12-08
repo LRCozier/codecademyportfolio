93135d6d6161ed8172c169c8258c946ceb939d0b
LRCozier <lrcozier@gmail.com>
2023-12-08 14:33:00 +0000

    updated index.html and styles.css

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // make only one section visible at a time
 document.addEventListener('DOMContentLoaded', () => {
-    const sitesections = document.querySelector('.sections section') // get all sections on site
+    const sitesections = document.querySelector('section') // get all sections on site
 });
 
 //add a click event listener for each section on site
 section.forEach(section => {
     section.addEventListener('click'), () => {
         // remove the active class from the current section
-        section.forEach(s => section.ClassList.remove('activesection'));
+        section.forEach(s => section.IdList.remove('mainsection'));
 
         //assign the active class to the new, selected section
-        section.ClassList.add('activesection');
+        section.ClassList.add('mainsection');
     };
     
 });
